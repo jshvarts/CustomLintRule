21bfcbd455c8680f58f210f6fed0eec8161de783
James Shvarts <jshvarts@gmail.com>
2020-01-04 16:33:51 -0500

    implement custom lint detector

--- app/src/main/java/com/example/customlintrule/MainActivity.kt
+++ app/src/main/java/com/example/customlintrule/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.customlintrule
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "testLog"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "testing")
     }
 }
